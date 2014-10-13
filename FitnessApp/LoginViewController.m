224a00b8bc2ccd3fbc36d8639b0ecd1c4ed3f3ec
wtchou712 <wtchou712@gmail.com>
2014-10-12 22:13:58 -0500

    fixed build errors

--- FitnessApp/LoginViewController.m
+++ FitnessApp/LoginViewController.m
@@ -8,19 +8,75 @@
 
 #import "LoginViewController.h"
 #import "WorkoutProgramListTableViewController.h"
+#import "WorkoutProgram.h"
+#import "WorkoutDay.h"
+#import "WorkoutExercise.h"
+#import "WorkoutSet.h"
 #import <Parse/Parse.h>
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @interface LoginViewController ()
 
+@property NSMutableArray *WorkoutPrograms;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *createAccountButton;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
 @implementation LoginViewController
+
+
+- (void)loadUserData
+{
+    //query for the WorkoutPrograms by the current user
+    PFQuery *query = [PFQuery queryWithClassName:@"WorkoutPrograms"];
+    [query whereKey:@"createdBy" equalTo:[PFUser currentUser]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *programs, NSError *error) {
+        if (!error)
+        {
+            //create all the WorkoutPrograms from the data pulled from parse
+            for (int i = 0; i < [programs count]; i++)
+            {
+                PFObject *currProgram = [programs objectAtIndex:i];
+                
+                //create the current program
+                WorkoutProgram *addedProgram = [[WorkoutProgram alloc] init];
+                addedProgram.programName = currProgram[@"programName"];
+                
+                //get the days for that program from arse
+                NSMutableArray *programDays = [[NSMutableArray alloc] init];
+                programDays = [currProgram objectForKey:@"workoutDayNames"];
+                
+                //initialize the array for the workout days of the program
+                addedProgram.workoutDays = [[NSMutableArray alloc]init];
+                
+                //create the workout days for each Program
+                for(int k = 0 ; k < [programDays count]; k++)
+                {
+                    //make the WorkoutDay
+                    WorkoutDay *addedDay = [[WorkoutDay alloc]init];
+                    addedDay.dayName = [programDays objectAtIndex:k];
+                    
+                    //add the WorkoutDay to the program
+                    [addedProgram.workoutDays addObject:addedDay];
+                }
+                
+                //add the program to the array of programs
+                [self.WorkoutPrograms addObject:addedProgram];
+                
+            }
+        }
+        else
+        {
+            NSLog(@"Error");
+        }
+    }];
+
+}
 
 
 - (IBAction)loginAction:(id)sender
@@ -85,6 +141,18 @@
     {
         NSLog(@"user is logged in");
         NSLog(@"%@", currentUser.username);
+        
+        //hide the textfields and buttons, if the user is already logged in
+        //show activity indicator and begin querying all data
+        [self.usernameTextField setHidden:YES];
+        [self.passwordTextField setHidden:YES];
+        [self.loginButton setHidden:YES];
+        [self.createAccountButton setHidden:YES];
+        [self.activityIndicator setHidden:NO];
+        [self.activityIndicator startAnimating];
+        
+        
+        
         
         [self performSegueWithIdentifier:@"loginToProgram" sender:self];
         
