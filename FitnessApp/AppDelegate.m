abd19017422a0cbd168b165a61ae10e4536e19ca
wtchou712 <wtchou712@gmail.com>
2014-11-07 16:36:41 -0600

    implementing Facebook login, still needs work

--- FitnessApp/AppDelegate.m
+++ FitnessApp/AppDelegate.m
@@ -8,8 +8,14 @@
 
 #import "AppDelegate.h"
 #import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 @implementation AppDelegate
+
+
+
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -22,8 +28,18 @@
     
     [Parse setApplicationId:@"m3SKNtM4jaARFcywoDStmTyC422M7a94gLoHgna9"
                   clientKey:@"pUHieNHBgvQa0ClpsxazhC6R3VBpoNMoLSvr0p9g"];
+    [PFFacebookUtils initializeFacebook];
     
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -46,11 +62,13 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[PFFacebookUtils session] close];
 }
 
 @end
--- FitnessApp/LoginViewController.m
+++ FitnessApp/LoginViewController.m
@@ -13,6 +13,7 @@
 #import "WorkoutExercise.h"
 #import "WorkoutSet.h"
 #import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
@@ -25,216 +26,58 @@
 @property (weak, nonatomic) IBOutlet UIButton *createAccountButton;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
+
 @end
 
 @implementation LoginViewController
 
 
-- (void)loadUserData
-{
-    //query for the WorkoutPrograms by the current user
-    PFQuery *query = [PFQuery queryWithClassName:@"WorkoutPrograms"];
-    [query whereKey:@"createdBy" equalTo:[PFUser currentUser]];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *programs, NSError *error) {
-        if (!error)
-        {
-            //create all the WorkoutPrograms from the data pulled from parse
-            for (int i = 0; i < [programs count]; i++)
-            {
-                PFObject *currProgram = [programs objectAtIndex:i];
-                
-                //create the current program
-                WorkoutProgram *addedProgram = [[WorkoutProgram alloc] init];
-                addedProgram.programName = currProgram[@"programName"];
-                
-                //get the days for that program from arse
-                NSMutableArray *programDays = [[NSMutableArray alloc] init];
-                programDays = [currProgram objectForKey:@"workoutDayNames"];
-                
-                //initialize the array for the workout days of the program
-                addedProgram.workoutDays = [[NSMutableArray alloc]init];
-                
-                //create the workout days for each Program
-                for(int j = 0 ; j < [programDays count]; j++)
-                {
-                    //make the WorkoutDay
-                    WorkoutDay *addedDay = [[WorkoutDay alloc]init];
-                    addedDay.dayName = [programDays objectAtIndex:j];
-                    
-                    //get the selected Day
-                    //retrieve the exercises for that Day
-                    //create WorkoutExercises from the query result
-                    //add the exercises to the array of exercises for that day
-                    
-                    NSString *currentDayWithoutSpaces = [addedDay.dayName stringByReplacingOccurrencesOfString:@" " withString:@""];
-                    PFQuery *query = [PFQuery queryWithClassName:currentDayWithoutSpaces];
-                    
-                    [query getFirstObjectInBackgroundWithBlock:^(PFObject *day, NSError *error) {
-                        if (!error)
-                        {
-                            //successful find
-                            NSLog(@"Successfully retrieved the exercises");
-                            
-                            //get all the exercises names for the day from parse
-                            NSMutableArray *dayExercises = [[NSMutableArray alloc] init];
-                            dayExercises = [day objectForKey:@"exerciseNames"];
-                            
-//                            //initialize the array for the workout exercises of the day
-//                            addedDay.workoutExercises = [[NSMutableArray alloc] init];
-                            
-                            //create the workout exercises for each Program
-                            for(int k = 0 ; k < [dayExercises count]; k++)
-                            {
-                                //make the WorkoutExercises
-                                WorkoutExercise *addedExercise = [[WorkoutExercise alloc]init];
-                                addedExercise.exerciseName = [dayExercises objectAtIndex:k];
-                                
-                                NSString *selectedDayWithoutSpaces = [addedExercise.exerciseName stringByReplacingOccurrencesOfString:@" " withString:@""];
-                                PFQuery *query = [PFQuery queryWithClassName:selectedDayWithoutSpaces];
-                                
-                                [query getFirstObjectInBackgroundWithBlock:^(PFObject *day, NSError *error) {
-                                    if (!error)
-                                    {
-                                        //successful find
-                                        NSLog(@"Successfully retrieved the exercises");
-                                        
-                                        //get all the exercises names for the day from parse
-                                        NSMutableArray *dayExercises = [[NSMutableArray alloc] init];
-                                        dayExercises = [day objectForKey:@"exerciseNames"];
-                                        
-                                        //initialize the array for the workout exercises of the day
-                                        addedDay.workoutExercises = [[NSMutableArray alloc] init];
-                                        
-                                        //create the workout exercises for each Program
-                                        for(int m = 0 ; m < [dayExercises count]; m++)
-                                        {
-                                            //make the WorkoutExercises
-                                            //WorkoutExercise *addedExercise = [[WorkoutExercise alloc]init];
-                                            addedExercise.exerciseName = [dayExercises objectAtIndex:m];
-                                            addedExercise.repCount = @"";
-                                            addedExercise.setCount = @"";
-                                            
-//                                            //get the repCount and setCount from
-//                                            //-----query for the exercise class-----
-//                                            NSString *exerciseNameWithoutSpaces = [addedExercise.exerciseName stringByReplacingOccurrencesOfString:@" " withString:@""];
-//                                            PFQuery *repsetQuery = [PFQuery queryWithClassName:exerciseNameWithoutSpaces];
-//                                            
-//                                            [repsetQuery getFirstObjectInBackgroundWithBlock:^(PFObject *exercise, NSError *error) {
-//                                                if(!error)
-//                                                {
-//                                                    NSLog(@"Successfully retrieved the set and rep counts");
-//                                                    addedExercise.repCount = exercise[@"repCount"];
-//                                                    addedExercise.setCount = exercise[@"setCount"];
-//                                                }
-//                                                else
-//                                                {
-//                                                    NSLog(@"Error");
-//                                                }
-//                                                
-//                                                
-//                                            }];
-                                            
-                                            //get the exercise name without spaces
-                                            NSString *exerciseNameWithoutSpaces = [addedExercise.exerciseName stringByReplacingOccurrencesOfString:@" " withString:@""];
-                                            
-                                            //begin a query to get that exercise class
-                                            PFQuery *query = [PFQuery queryWithClassName:exerciseNameWithoutSpaces];
-                                            
-                                            //get all objects except the one that has dateCompleted = "N/A"
-                                            [query whereKey:@"dateCompleted" notEqualTo:@"N/A"];
-                                            
-                                            [query findObjectsInBackgroundWithBlock:^(NSArray *sets, NSError *error) {
-                                                if(!error)
-                                                {
-                                                    //initialize array of WorkoutSets
-                                                    addedExercise.workoutSets = [[NSMutableArray alloc] init];
-                                                    
-                                                    for(int p = 0; p <[sets count]; p++)
-                                                    {
-                                                        //get the log of the exercise for one day
-                                                        PFObject *object = [sets objectAtIndex:p];
-                                                        NSMutableArray *repArray = [[NSMutableArray alloc]init];
-                                                        NSMutableArray *weightArray = [[NSMutableArray alloc]init];
-                                                        repArray = [object objectForKey:@"repArray"];
-                                                        weightArray = [object objectForKey:@"weightArray"];
-                                                        
-                                                        //create a WorkoutSet for each rep and weight for that date
-                                                        for(int a= 0; a < [repArray count]; a++)
-                                                        {
-                                                            //create the WorkoutSet
-                                                            WorkoutSet *addedSet = [[WorkoutSet alloc]init];
-                                                            addedSet.rep = [repArray objectAtIndex:a];
-                                                            addedSet.weight = [repArray objectAtIndex:a];
-                                                            addedSet.dateCompleted = [object objectForKey:@"dateCompleted"];
-                                                            
-                                                            NSLog(@"rep: ");
-                                                            NSLog(@"%@", addedSet.rep);
-                                                            NSLog(@"weight: ");
-                                                            NSLog(@"%@", addedSet.weight);
-                                                            
-                                                            //add it to the array of WorkoutSets
-                                                            [addedExercise.workoutSets addObject:addedSet];
-                                                        }
-                                                    }
-                                                }
-                                                else
-                                                {
-                                                    NSLog(@"Error");
-                                                }
-                                                //self.workoutSets = [NSMutableArray arrayWithArray:self.selectedExercise.workoutSets];
-                                                //[self organizeSetsDates];
-                                                //[self.tableView reloadData];
-                                            }];
-                                            
-                                            
-                                            
-                                            //-----finish query for the exercise class-----
-                                            
-                                            
-//                                            //add the WorkoutExercise to the program
-//                                            [addedDay.workoutExercises addObject:addedExercise];
-                                        }
-                                        //self.exercises = [NSMutableArray arrayWithArray:self.selectedDay.workoutExercises];
-
-                                    }
-                                    else
-                                    {
-                                        NSLog(@"Error");
-                                    }
-                                }];
-                                //add the WorkoutExercise to the program
-                                [addedDay.workoutExercises addObject:addedExercise];
-                            }
-                            //[self.tableView reloadData];
-                        }
-                        else
-                        {
-                            NSLog(@"Error");
-                        }
-                    }];
-                    
-                    //add the WorkoutDay to the program
-                    [addedProgram.workoutDays addObject:addedDay];
-                }
-                
-                //add the program to the array of programs
-                [self.WorkoutPrograms addObject:addedProgram];
-                
-            }
-        }
-        else
-        {
-            NSLog(@"Error");
-        }
-    }];
-    [self.activityIndicator stopAnimating];
-    [self.activityIndicator setHidden:YES];
-
-}
-
-
 - (IBAction)loginAction:(id)sender
 {
+//    [PFFacebookUtils logInWithPermissions:permissions block:^(PFUser *user, NSError *error) {
+//        if (!user) {
+//            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+//        } else if (user.isNew) {
+//            NSLog(@"User signed up and logged in through Facebook!");
+//        } else {
+//            NSLog(@"User logged in through Facebook!");
+//        }
+//    }];
+    
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+    
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        [_activityIndicator stopAnimating]; // Hide loading indicator
+        
+        if (!user) {
+            NSString *errorMessage = nil;
+            if (!error) {
+                NSLog(@"Uh oh. The user cancelled the Facebook login.");
+                errorMessage = @"Uh oh. The user cancelled the Facebook login.";
+            } else {
+                NSLog(@"Uh oh. An error occurred: %@", error);
+                errorMessage = [error localizedDescription];
+            }
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error"
+                                                            message:errorMessage
+                                                           delegate:nil
+                                                  cancelButtonTitle:nil
+                                                  otherButtonTitles:@"Dismiss", nil];
+            [alert show];
+        } else {
+            if (user.isNew) {
+                NSLog(@"User with facebook signed up and logged in!");
+            } else {
+                NSLog(@"User with facebook logged in!");
+            }
+        }
+    }];
+    
+    [_activityIndicator startAnimating]; // Show loading indicator until login is finished
+    
+    /*
     NSString *enteredUsername = self.usernameTextField.text;
     NSString *enteredPassword = self.passwordTextField.text;
     
@@ -249,6 +92,7 @@
                                             NSLog(@"login failed");
                                         }
                                     }];
+     */
 }
 
 - (IBAction)logoutUser:(UIStoryboardSegue *) segue
@@ -295,19 +139,6 @@
     {
         NSLog(@"user is logged in");
         NSLog(@"%@", currentUser.username);
-        
-        //hide the textfields and buttons, if the user is already logged in
-        //show activity indicator and begin querying all data
-//        [self.usernameTextField setHidden:YES];
-//        [self.passwordTextField setHidden:YES];
-//        [self.loginButton setHidden:YES];
-//        [self.createAccountButton setHidden:YES];
-//        [self.activityIndicator setHidden:NO];
-//        [self.activityIndicator startAnimating];
-//        [self loadUserData];
-        
-        
-        
         
         [self performSegueWithIdentifier:@"loginToProgram" sender:self];
         
